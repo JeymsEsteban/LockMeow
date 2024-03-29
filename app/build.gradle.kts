9e197cd9e6263c160570dbe77bb276750b2766d3
JeymsEsteban <retsag878@gmail.com>
2024-03-29 17:10:56 -0500

    Adding SplashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,8 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.core.splashscreen)
+    implementation(libs.core.splashscreen.v100)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
@@ -40,4 +41,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
 }
