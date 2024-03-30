89f7f569811bac74c92bab85f43d445152fd6031
JeymsEsteban <retsag878@gmail.com>
2024-03-29 19:00:29 -0500

    SlpashScreen(Falta ajustar el icono)

--- app/src/main/java/com/example/lockmeow/MainActivity.java
+++ app/src/main/java/com/example/lockmeow/MainActivity.java
@@ -1,22 +1,39 @@
 package com.example.lockmeow;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewTreeObserver;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Boolean isReady = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+        View content =  findViewById(android.R.id.content);
+        content.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                if (isReady){
+                    content.getViewTreeObserver().removeOnPreDrawListener(this);
+                }
+                dissmisSplashScreen();
+                return false;
+            }
+        });
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+    }
+    private void dissmisSplashScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                isReady = true;
+            }
+        }, 1000);
     }
 }
