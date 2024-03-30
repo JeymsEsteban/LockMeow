75ee4d5f19e6b629f24487e8455be44e956539a4
JeymsEsteban <retsag878@gmail.com>
2024-03-30 13:58:15 -0500

    Icono ajustado y codigo Main comentado

--- app/src/main/java/com/example/lockmeow/MainActivity.java
+++ app/src/main/java/com/example/lockmeow/MainActivity.java
@@ -12,13 +12,17 @@ public class MainActivity extends AppCompatActivity {
     Boolean isReady = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Jeyms: Instalar y mostrar el splash screen
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         View content =  findViewById(android.R.id.content);
+        // Jeyms: Agregar un listener para detectar cuando la vista esté lista para ser dibujada
         content.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
             @Override
             public boolean onPreDraw() {
+                //Jeyms: Verificar si el contenido esta listo
                 if (isReady){
                     content.getViewTreeObserver().removeOnPreDrawListener(this);
+                    // Jeyms: Si está listo, eliminar el listener
                 }
                 dissmisSplashScreen();
                 return false;
@@ -29,9 +33,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void dissmisSplashScreen() {
+        // Jeyms: Retrasar la actualizacion del estado isReady
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
+                // Jeyms: Establecer isReady en true despues de 1000 milisegundos (1 segundo)
                 isReady = true;
             }
         }, 1000);
