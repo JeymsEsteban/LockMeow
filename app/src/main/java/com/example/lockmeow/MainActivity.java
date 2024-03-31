e211ecd646a40b036220be46abbaedf15381ff22
JeymsEsteban <retsag878@gmail.com>
2024-03-30 19:43:28 -0500

    Implementacion del login, falta un diseño basico y meramente estetico del home, tambien quitar "Click para registrarse" que esta en el registro tambien

--- app/src/main/java/com/example/lockmeow/MainActivity.java
+++ app/src/main/java/com/example/lockmeow/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.lockmeow;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.ViewTreeObserver;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.splashscreen.SplashScreen;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseAuth auth;
+    Button button;
+    TextView textView;
+    FirebaseUser user;
     Boolean isReady = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +40,27 @@ public class MainActivity extends AppCompatActivity {
         });
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.logout);
+        textView = findViewById(R.id.user_details);
+        user = auth.getCurrentUser();
+        if (user == null) {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+        else {
+            textView.setText(user.getEmail());
+        }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
     private void dissmisSplashScreen() {
         // Jeyms: Retrasar la actualizacion del estado isReady
